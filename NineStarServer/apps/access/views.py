0e0a539d91ebe00edd1e614dfefbd876308af051
Alberto Sanchez <asanchez@talpor.com>
2013-04-28 16:45:13 -0430

    plug api with gpio module

--- NineStarServer/apps/access/views.py
+++ NineStarServer/apps/access/views.py
@@ -1,12 +1,18 @@
 # Create your views here.
 from NineStarServer.helpers.http import JsonResponse
 
+from utils import gpio
+
 
 def access_action(request, gate):
     """Try run the action script for the given gate"""
-    if gate == '1':
-        return JsonResponse({'success': True})
-    elif gate == '2':
-        return JsonResponse({'success': True})
-    else:
-        return JsonResponse({'success': False})
+    sleep = 1
+    success = False
+    if gate == '1' or gate == '2':
+        if gate == '1':
+            sleep = 5
+
+        gpio.puss_button(gate, sleep)
+        success = True
+
+    return JsonResponse({'success': success})
