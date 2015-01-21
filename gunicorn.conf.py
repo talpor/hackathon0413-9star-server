816a50237dbdf154f67a167bca4a8486c4e43989
Daniel Barreto <dbarreto@talpor.com>
2015-01-20 22:36:40 -0430

    Adds gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,16 @@
+import os
+
+def num_cpus():
+    if not hasattr(os, "sysconf"):
+        raise RuntimeError("No sysconf detected.")
+        return os.sysconf("SC_NPROCESSORS_ONLN")
+
+
+preload = True
+workers = num_cpus() * 2 + 1
+bind = '127.0.0.1:8000'
+
+# log files
+accesslog = 'logs/gunicorn-access.log'
+errorlog  = 'logs/gunicorn-error.log'
+loglevel  = 'debug'
